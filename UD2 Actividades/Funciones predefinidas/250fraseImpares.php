c8a1054c38752fd36b8c43a9101ed3afbf8ef124
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-10-07 14:20:18 +0200

    funciones predefinidas

---
+++ UD2 Actividades/Funciones predefinidas/250fraseImpares.php
@@ -0,0 +1,26 @@
+<?php 
+
+        echo "
+            <form action='' method='get'>
+                <input placeholder='Escribe una frase' name='frase'>
+                <button type='submit'>Enviar</button>
+            </form>
+        ";
+
+        if (isset($_GET['frase'])) {
+            $frase = $_GET['frase'];
+
+            $caracteres = str_split($frase);
+            $longitud = count($caracteres);
+        
+            for ($i=0; $i < $longitud; $i++) { 
+                
+                if ($i%2 == 0) {
+                    echo $caracteres[$i];
+                }
+
+            }
+            
+        }
+
+?>
---
+++ UD2 Actividades/Funciones predefinidas/251vocales.php
@@ -0,0 +1,47 @@
+<?php
+
+echo "
+    <form action='' method='get'>
+        <input placeholder='Escribe una frase' name='frase'>
+        <button type='submit'>Enviar</button>
+    </form>
+";
+
+if (isset($_GET['frase'])) {
+
+    $frase = $_GET['frase'];
+    $caracteres = str_split($frase);
+    $longitud = count($caracteres);
+
+    $acc= 0;
+    $ecc=0;
+    $icc=0;
+    $occ=0;
+    $ucc=0;
+    
+    foreach($caracteres as $car){
+
+        if ($car == "a") {
+            $acc++;
+        } elseif ($car == "e"){
+            $ecc++;
+        } elseif ($car == "i"){
+            $icc++;
+        } elseif ($car == "o"){
+            $occ++;
+        } elseif ($car == "u"){
+            $ucc++;
+        }
+
+    }
+    $total = $acc + $ecc + $icc + $occ + $ucc;
+    echo "A: " . $acc . "<br>";
+    echo "E: " . $ecc . "<br>";
+    echo "I: " . $icc . "<br>";
+    echo "O: " . $occ . "<br>";
+    echo "U: " . $ucc . "<br>";
+
+    echo "Total: " . $total;
+}
+
+?>
