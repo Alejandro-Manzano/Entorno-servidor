688869ecfcdfaa78ca55dd9deccd2da9bf203eb1
Alejandro Manzano <alejandromanzano.laforga@gmail.com>
2024-10-08 08:46:33 +0200

    250 impares

--- UD2 Actividades/Funciones predefinidas/250fraseImpares.php
+++ UD2 Actividades/Funciones predefinidas/250fraseImpares.php
@@ -1,26 +1,26 @@
-<?php 
+<?php
 
-        echo "
-            <form action='' method='get'>
-                <input placeholder='Escribe una frase' name='frase'>
-                <button type='submit'>Enviar</button>
-            </form>
-        ";
+function procesarFrase($frase) {
+   
+    $caracteres = str_split($frase);
+    $longitud = count($caracteres);
 
-        if (isset($_GET['frase'])) {
-            $frase = $_GET['frase'];
-
-            $caracteres = str_split($frase);
-            $longitud = count($caracteres);
-        
-            for ($i=0; $i < $longitud; $i++) { 
-                
-                if ($i%2 == 0) {
-                    echo $caracteres[$i];
-                }
-
-            }
-            
+    for ($i = 0; $i < $longitud; $i++) {
+        if ($i % 2 == 0) {
+            echo $caracteres[$i];
         }
+    }
+}
+
+echo "
+    <form action='' method='get'>
+        <input placeholder='Escribe una frase' name='frase'>
+        <button type='submit'>Enviar</button>
+    </form>
+";
+
+if (isset($_GET['frase'])) {
+    procesarFrase($_GET['frase']);
+}
 
 ?>
